c51863bb2bb268a3b97d2ab15253a2cf9cb0f57d
aljedaxi@gmail.com <aljedaxi@gmail.com>
2019-05-27 01:12:44 -0600

    refactored code to rely less on globals

--- build_edition.py
+++ build_edition.py
@@ -15,27 +15,27 @@ from    bookletting import booklet
 
 #template_file = ""
 
-CONF = yaml.load(open('vars.yml').read())
-GLOBAL_CONF = yaml.load(open('global_vars.yml').read())
+#where to write formatted things when they don't
+DEFAULTS = "failed_formattings.tex"
 #TODO: find a way to externalize this code
 ENV = jinja2.Environment(
-    block_start_string	    = '\BLOCK{',
-    block_end_string		= '}',
-    variable_start_string	= '\VAR{',
-    variable_end_string	    = '}',
-    comment_start_string	= '%/*',
-    comment_end_string	    = '%*/',
-    line_comment_prefix	    = '%//',
-    line_statement_prefix	= '%%',
-    trim_blocks 			= True,
-    lstrip_blocks			= True,
-    autoescape			    = False,
-    loader				    = jinja2.FileSystemLoader("./"),
+    block_start_string   =r'\BLOCK{',
+    block_end_string     ='}',
+    variable_start_string=r'\VAR{',
+    variable_end_string  ='}',
+    comment_start_string ='%/*',
+    comment_end_string   ='%*/',
+    line_comment_prefix  ='%//',
+    line_statement_prefix='%%',
+    trim_blocks          =True,
+    lstrip_blocks        =True,
+    autoescape           =False,
+    loader               =jinja2.FileSystemLoader("./templates"),
 )
 
 def cleanup(title, minutes):
     """
-        removes all of the files created by LaTeX, 
+        removes all of the files created by LaTeX,
         then moves the original minutes file into the archives.
     """
     call(("rm",
@@ -62,7 +62,7 @@ def fill(template_file, outfile, meta):
 
     template = ENV.get_template(template_file)
 
-    #etwas = Popen(("pdflatex"), stdout=PIPE, 
+    #etwas = Popen(("pdflatex"), stdout=PIPE,
     #              stdin=PIPE, stderr=PIPE).communicate(
     #                                                   input=bytes(
     #                                                         template.render(
@@ -81,10 +81,10 @@ def fill(template_file, outfile, meta):
 
     call(("latexmk", "--pdf", outfile))
 
-def format_articles(articles):
+def format_articles(articles, force=False, verbose=False):
     """
         takes the list of articles---as defined in vars.yml---and
-        if they haven't yet been formatted, formats them 
+        if they haven't yet been formatted, formats them
         (using the code in types_and_settings/ and templates in templates/)
     """
     f_files = []
@@ -99,8 +99,8 @@ def format_articles(articles):
         #generate outfile filename; test for existence
         (directory, a_file) = article['file'].split("/")
         outfile = "/".join((directory, f"f_{a_file}"))
-        if path.isfile(outfile) and not FORCE:
-            if VERBOSE:
+        if path.isfile(outfile) and not force:
+            if verbose:
                 print(f"{outfile} already exists. Skipping {article['title']}")
             #TeX likes its input files implicitly extended
             f_files.append(outfile[:-4])
@@ -111,12 +111,33 @@ def format_articles(articles):
         if not function:
             raise NotImplementedError(f"Function {function} not implemented.")
         formatted_article = function.main(text, meta=article, env=ENV)
-        #TODO: put this in try except
-        open(outfile, "w").write(formatted_article)
-        if VERBOSE:
+        try:
+            open(outfile, "w").write(formatted_article)
+        except:
+            print(f"writing to {outfile} failed. Writing to {DEFAULTS} instead.")
+            open(DEFAULTS, "a").write(formatted_article)
+
+        if verbose:
             print(f"writing formatted {article['title']} text to {outfile}")
         f_files.append(outfile[:-4])
     return f_files
+
+def main(force=False, verbose=False):
+    CONF = yaml.load(open('vars.yml').read())
+    GLOBAL_CONF = yaml.load(open('global_vars.yml').read())
+
+    META = CONF['conf']
+
+    #compiles each article from txt into LaTeX,
+    #using the templating systems defined in types_and_settings
+    F_FILES = format_articles(CONF['protein'], force=force, verbose=verbose)
+
+    META['files'] = F_FILES
+    TEMPLATE_FILE = META['template']
+    OUTFILE_CORE = f"{GLOBAL_CONF['zine_title']}_zine_{META['edition']}"
+
+    fill(TEMPLATE_FILE, f"{OUTFILE_CORE}.tex", META)
+    booklet(OUTFILE_CORE, f"{GLOBAL_CONF['zine_title']}_booklet_{META['edition']}")
 
 if __name__ == "__main__":
     import argparse
@@ -125,21 +146,4 @@ if __name__ == "__main__":
     parser.add_argument("-f", "--force",   help="force recreation of articles", action="store_true")
     ARGS = parser.parse_args()
 
-    #from sys import argv
-
-    #if this is set, the program will rebuild articles that have already been processed
-    FORCE = ARGS.force
-    VERBOSE = ARGS.verbose
-
-    META = CONF['conf']
-
-    #compiles each article from txt into LaTeX, 
-    #using the templating systems defined in types_and_settings
-    _f_files = format_articles(CONF['protein'])
-
-    META['files'] = _f_files
-    TEMPLATE_FILE = META['template']
-    outfile_core = f"{GLOBAL_CONF['zine_title']}_zine_{META['edition']}"
-
-    fill(TEMPLATE_FILE, f"{outfile_core}.tex", META)
-    booklet(outfile_core, f"{GLOBAL_CONF['zine_title']}_booklet_{META['edition']}")
+    main(force=ARGS.force, verbose=ARGS.verbose)
