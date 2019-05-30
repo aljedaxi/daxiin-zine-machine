4d7eeda819be6ac429ac11a4d53864143132131f
daxi <aljedaxi@gmail.com>
2019-05-30 02:25:50 -0600

    added a custom class

---
+++ types_and_settings/custom.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+    this takes something and assumes the source file has it's own internal typesetting.
+    mostly this is for stuff that daxi's written.
+"""
+
+from   jinja2 import Environment
+from  .render import fill
+
+def main(text, meta={}, env=Environment()):
+    """
+        this is the driver for custom typesetting.
+        text is the custom text. It can have any variables that get set in vars.yml.
+        meta is metadata, eg, author, title, bio.
+        env is a jinja2 environment.
+    """
+    meta['text'] = text
+    lines_out = fill("custom.tex", env=env, meta=meta)
+    return lines_out
+
+if __name__ == "__main__":
+    PROSE = open("my_fucking_restaurant.tex").read()
+    print(main(PROSE))
+        #insert each one into the index
+
